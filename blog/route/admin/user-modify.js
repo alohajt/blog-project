9185d2a240564ddc1aa1e7d65041fb8342dae1ca
alohajt <alohajiatao@gmail.com>
2020-07-31 21:02:08 -0600

    password compare pass

--- blog/route/admin/user-modify.js
+++ blog/route/admin/user-modify.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt')
 
 module.exports = async (req, res, next) => {
     // receive request params from client
-    const body = req.body
+    const {username, email, role, state} = req.body
     // id of user who will modify
     const id = req.query.id
 
@@ -12,13 +12,20 @@ module.exports = async (req, res, next) => {
     const isValid = await bcrypt.compare(req.body.password, user.password)
     if (isValid) {
         // password compare pass
-        res.send('yes')
+        //res.send('yes')
+        // update user data into database
+        await User.updateOne({ _id: id }, {
+            username: username,
+            email: email,
+            role: role,
+            state: state
+        })
+        // redirect to user list
+        res.redirect('/admin/user')
+
     } else {
         // password compare fail
         let obj = {path:'/admin/user-edit', message: 'Wrong password, please enter again', id: id}
         next(JSON.stringify(obj))
     }
-
-    // compare password
-    // res.send(user)
 }
