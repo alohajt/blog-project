7df484790d4538947900fcddd7b7b58424095ae7
alohajt <alohajiatao@gmail.com>
2020-08-05 14:09:33 -0600

    user delete func done

--- blog/route/admin.js
+++ blog/route/admin.js
@@ -26,7 +26,20 @@ admin.get('/user-edit', require('./admin/user-edit'))
 // create route to realize user add
 admin.post('/user-edit', require('./admin/user-edit-fn'))
 
+// create route to edit user
 admin.post('/user-modify', require('./admin/user-modify'))
+
+// 用户删除功能路由
+admin.get('/delete', require('./admin/user-delete'));
+
+// // 文章列表页面路由
+// admin.get('/article', require('./admin/article'));
+
+// // 文章编辑页面路由
+// admin.get('/article-edit', require('./admin/article-edit'));
+
+// // 实现文章添加功能的路由
+// admin.post('/article-add', require('./admin/article-add'))
 
 //export route object as a module
 module.exports = admin;
